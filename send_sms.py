cbfb62ed3eeabb46efa09b92b6fb5e758836e7bb
Luke Kerbs <luke@Lukes-MBP-3.lan>
2022-03-06 20:52:18 -0800

    security patch

--- send_sms.py
+++ send_sms.py
@@ -1,8 +1,8 @@
 # Test twilio to sms
 from twilio.rest import Client
 
-account_sid = "AC3bf727d99cb53f9942d00a30d50d4f54"
-auth_token  = "5ff7f66c4ea4a02bfcf5b98eab0fa81b"
+account_sid = "xxxxxx"
+auth_token  = "xxxxxx"
 
 client = Client(account_sid, auth_token)
 
